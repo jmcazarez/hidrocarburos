c2cf5ab30039b14b1e851ce99a81d34ee7a519e5
romerojoseing <romerojoseing@gmail.com>
2022-07-22 17:05:26 +0200

    Consulta Banco

---
+++ src/app/views/pages/consult-bank/consult-bank.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-consult-bank',
+  templateUrl: './consult-bank.component.html',
+  styleUrls: ['./consult-bank.component.scss'],
+  preserveWhitespaces: true
+})
+export class ConsultBankComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}

708f0865cf9615086aa1aa31ff2019a6d368e618
Heber Josue <hebernevarez@gmail.com>
2022-08-16 15:22:48 -0700

    scripts para limpiar tablas

---
+++ Scripts/limpiar_tablas.sql
@@ -0,0 +1,20 @@
+delete from ctl_almacenes;
+delete from ctl_articulos;
+delete from ctl_clientes;
+delete from ctl_conceptos_de_gastos;
+delete from ctl_cuentas_bancarias;
+delete from ctl_empresas;
+delete from ctl_proveedores;
+delete from ctl_sucursales;
+
+
+
+ALTER TABLE ctl_almacenes AUTO_INCREMENT = 1;
+ALTER TABLE ctl_articulos AUTO_INCREMENT=1;
+ALTER TABLE ctl_clientes AUTO_INCREMENT=1;
+ALTER TABLE ctl_conceptos_de_gastos AUTO_INCREMENT=1;
+ALTER TABLE ctl_cuentas_bancarias AUTO_INCREMENT=1;
+ALTER TABLE ctl_empresas AUTO_INCREMENT=1;
+ALTER TABLE ctl_proveedores AUTO_INCREMENT=1;
+ALTER TABLE ctl_sucursales AUTO_INCREMENT=1;
+
